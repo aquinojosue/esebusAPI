47d90b9dccfa11f17b0cc43c62f13e08fcc010af
Josue Aquino <aquinojosue3@gmail.com>
2021-02-24 18:33:44 -0600

    cambios para 404

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const rutasJson = require('./rutas.json')
 const app = express(); 
 
 app.get('/', function(req, res) {
-    res.json(rutasJson.map(a=>a.nombreRuta))
+    res.json(rutasJson.map(a=>[a.nombreRuta, a.colorIda]))
     console.log("El total es: " + rutasJson.length)
 })
 
-app.get('/:nombreRuta', function(req, res){
+app.get('/ruta/:nombreRuta', function(req, res){
     const result = rutasJson.filter( a=>
         a.nombreRuta.toLowerCase().includes(req.params.nombreRuta.toLowerCase())
     )
@@ -15,4 +15,7 @@ app.get('/:nombreRuta', function(req, res){
     console.log("El tamaño de la consulta es: "+result.length)
 })
 
+app.use(function (req,res,next){
+	res.status(404).json({error: 'No se encontro el recurso solicitado'});
+});
 module.exports = app;
