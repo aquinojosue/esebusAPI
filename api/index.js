2dd6ef83edc08e61be587411b49f1febbca0db83
Josue Aquino <aquinojosue3@gmail.com>
2021-03-28 15:00:10 -0600

    cambios y agregar consolidado

--- api/index.js
+++ api/index.js
@@ -1,29 +1,47 @@
+import { readFileSync } from 'fs'
+import { join } from 'path'
+var cors = require('cors')
+
+
 const app = require('express')()
+const file = readFileSync(join(__dirname, '_files', 'consolidado.json'), 'utf8')
 
-const { readFileSync } = require('fs')
-const { join } = require('path')
-const file = readFileSync(join(__dirname, '_files', 'rutas.json'), 'utf8')
-
-//Aqui van los json
+//Cargar el json desde la lectura del archivo anterior
 const rutasJson = JSON.parse(file)
 const errorJson = {error:'No se encontro el recurso solicitado'}
 
-app.get('/api', function(req, res) {
-  res.json(rutasJson.map(a=>[a.nombreRuta, a.codigoRuta]))
+app.use(cors())
+
+app.all('/api', function(req, res) {
+  res.json(rutasJson.map(a=>(
+    {
+      nombreRuta: a.nombreRuta,
+      codigoRuta: a.codigoRuta,
+      departamento: a.departamento
+    }
+  )))
 })
 
-app.get('/api/ruta', function(req, res){
+app.all('/api/ruta', function(req, res){
   var result = errorJson;
+  //Primer caso, si se necesita un codigo de rutas
   if(req.query.codigoRuta){
-      result = rutasJson.filter(a=> req.query.codigoRuta === a.codigoRuta)
-      result = (result.length>0) ? result[0]:errorJson;
-  } else
-  if(req.query.nombreRuta){
-      result = rutasJson.filter( a=> 
-          a.nombreRuta.toLowerCase().includes(req.query.nombreRuta.toLowerCase())
-      )
-      result = (result.length>0) ? result: errorJson;
+    result = rutasJson.filter(ruta => req.query.codigoRuta === ruta.codigoRuta)
   }
+  //Segundo caso, si se necesitan rutas por nombre
+  else if(req.query.nombreRuta){
+    result = rutasJson.filter( ruta => 
+      ruta.nombreRuta.toLowerCase().includes(req.query.nombreRuta.toLowerCase())
+    )
+  }
+  //Tercer caso, si se necesitan rutas por codigo, como requisito es un array
+  else if(req.query.rutas){
+    result = rutasJson.filter(ruta => req.query.rutas.includes(ruta.codigoRuta))
+  }
+  else if(req.query.bulk){
+    result = rutasJson;
+  }
+  result = (result.length>0) ? result: errorJson;
   res.json(result)
 })
 
@@ -31,4 +49,4 @@ app.use(function (req,res,next){
 	res.status(404).json(errorJson);
 });
 
-module.exports = app
+export default app
